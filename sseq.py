4d93ad9b7b42c35d90c0842b9b237a203fd1d1e8
Rob Bryan <rob@robertbryan.net>
2018-03-13 17:48:10 -0400

    Starting sseq

---
+++ sseq.py
@@ -0,0 +1,4 @@
+# Given: Two DNA strings s and t (each of length at most 1 kbp) in FASTA format.
+#
+# Return: One collection of indices of s in which the symbols of t appear
+# as a subsequence of s. If multiple solutions exist, you may return any one.
