e4ecb2ad075cf86b058cfe41df65397a05843401
Rob Bryan <rob@robertbryan.net>
2018-03-14 10:52:52 -0400

    solve tran

--- tran.py
+++ tran.py
@@ -15,7 +15,11 @@ import Bio.SeqIO
 
 dataset = []
 
-with open('./datasets/rosalind_tran_example.txt') as f:
+# with open('./datasets/rosalind_tran_example.txt') as f:
+#     for record in Bio.SeqIO.parse(f, 'fasta'):
+#         dataset.append(record.seq)
+
+with open(expanduser('~/Downloads/rosalind_tran.txt')) as f:
     for record in Bio.SeqIO.parse(f, 'fasta'):
         dataset.append(record.seq)
 
@@ -26,4 +30,17 @@ with open('./datasets/rosalind_tran_example.txt') as f:
 s1 = str(dataset[0])
 s2 = str(dataset[1])
 
-print(len(s1), len(s2))
+transitions = 0
+transversions = 0
+
+versions = set(['AT', 'AC', 'CG', 'GT'])
+
+for i in range(len(s1)):
+    a, b = s1[i], s2[i]
+    ab = ''.join(sorted(a + b))
+    if ab == 'AG' or ab == 'CT':
+        transitions += 1
+    elif ab in versions:
+        transversions += 1
+print(transitions/transversions)
+
