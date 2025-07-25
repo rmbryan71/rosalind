f706dd86d27912f5f427e837cbbef54df98d72d4
robertbryan <rob@robertbryan.net>
2025-07-25 18:25:25 -0400

    solves problem

--- solution-code/lia.py
+++ solution-code/lia.py
@@ -1,19 +1,16 @@
 from scipy.stats import binom
 import os
 
-def P(n, k):
-    return binom.cdf(n - 1, 2 ** k, 0.25)
-
-def lia(k, N):
-    return 1 - sum([P(n, k) for n in range(N)])
+def lia(k, n):
+    return sum(binom.pmf(x,2**k,0.25) for x in range(n,2**k+1))
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_lia.txt"
     solution_path = "../solution-outputs/rosalind_lia.txt"
     file = open(file_path, "r").readline().split(' ')
-    n = int(file[0])
-    k = int(file[1])
-    solution = str(P(n, k))
+    k = int(file[0])
+    n = int(file[1])
+    solution = str(lia(k, n))
     if os.path.exists(solution_path):
         os.remove(solution_path) #idempotent : https://en.wikipedia.org/wiki/Idempotence
     file = open(solution_path, "x")
