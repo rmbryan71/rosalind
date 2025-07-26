972f7adcf86e219a405b028b477ec35f0617abb3
robertbryan <rob@robertbryan.net>
2025-07-26 08:44:23 -0400

    solves problem

--- solution-code/grph.py
+++ solution-code/grph.py
@@ -2,22 +2,30 @@ import os
 from Bio import SeqIO
 
 def read_fasta(file):
-    records = SeqIO.parse(file, 'fasta')
-    return records
+    return list(SeqIO.parse(file, 'fasta'))
+
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_grph.txt"
     solution_path = "../solution-outputs/rosalind_grph.txt"
-    file = open(file_path, "r")
     ### Solving code goes here
-    records = read_fasta(file)
-    for record in records:
-        print(record.seq)
-    solution = str(0)
+    records = read_fasta(file_path)
+    solution = []
+
+    for record_a in records:
+        # print(record_a.seq)
+        for record_b in records:
+            #print(record_a.seq, record_b.seq)
+            # print(record_a.seq[0:3], record_b.seq[-3:])
+            if record_a.id != record_b.id:
+                if record_a.seq[0:3] == record_b.seq[-3:]:
+                    # print(record_a.seq[0:3], record_b.seq[-3:])
+                    solution.append(record_b.id + ' ' + record_a.id)
     ### End of solving code
     if os.path.exists(solution_path):
         os.remove(solution_path) #idempotent : https://en.wikipedia.org/wiki/Idempotence
     file = open(solution_path, "x")
-    file.write(solution)
+    for item in solution:
+        print(item)
+        file.write(item + '\n')
     file.close()
-    print(solution)
