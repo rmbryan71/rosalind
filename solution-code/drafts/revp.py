7d36f30d67b99f2fa206031393e71e789baa52f4
robertbryan <rob@robertbryan.net>
2025-07-31 18:55:24 -0400

    solves challenge

--- solution-code/drafts/revp.py
+++ solution-code/drafts/revp.py
@@ -1,28 +1,21 @@
 from Bio import SeqIO
-from Bio.Seq import Seq
-
-def read_fasta(file):
-    return list(SeqIO.parse(file, 'fasta'))
 
 def reverse_complement(string):
     return string.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()[::-1]
 
-def rev_pal(set, length):
-    results = []
-    for i in range(len(set) + 1 - length):
-        candidate = (set[i: i+length])
-        if candidate == reverse_complement(candidate):
-            results.append([i + 1, length])
-    return results
+def read_fasta(file):
+    handle = open(file)
+    record = list(SeqIO.parse(handle, "fasta"))
+    handle.close()
+    return str(record[0].seq)
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_revp.txt"
-    dna = open(file_path, 'r').read()
-    results = []
-    for j in range(4,12):
-        if len(rev_pal(dna, j)) > 0:
-            results.append(rev_pal(dna, j))
-    for item in results:
-        for i in item:
-            print(str(i).replace('[', '').replace(']', '').replace(',', ''))
+    # dna = open(file_path, 'r').read()
+    dna = read_fasta(file_path)
+    print(dna)
+    for i in range(len(dna)):
+        for j in range(4,13):
+            if i + j <= len(dna) and dna[i: i+j] == reverse_complement(dna[i: i+j]):
+                print(i+1, j)
 
