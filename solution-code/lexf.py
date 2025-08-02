ebbfca6c4d68fa037d86ef8d55a014b5bd495a29
robertbryan <rob@robertbryan.net>
2025-08-02 17:21:52 -0400

    solves challenge

--- solution-code/lexf.py
+++ solution-code/lexf.py
@@ -1,6 +1,14 @@
+import itertools
+
+def lexf(A, n):
+    for x in itertools.product(A, repeat = n):
+        print(str(''.join(x)), end = '\n')
+
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_lexf.txt"
     solution_path = "../solution-outputs/rosalind_lexf.txt"
     file = open(file_path, "r").readlines()
     A = str(file[0]).strip().replace(' ','')
     n = int(file[1])
+    lexf(A, n)
+
