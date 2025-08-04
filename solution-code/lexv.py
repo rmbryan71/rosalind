5826d70d986030a1fac8ead359dda30ef2b96312
robertbryan <rob@robertbryan.net>
2025-08-03 20:25:50 -0400

    solves challenge

--- solution-code/lexv.py
+++ solution-code/lexv.py
@@ -13,7 +13,7 @@ def lexv(A, n):
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_lexv.txt"
-    solution_path = "../../solution-outputs/rosalind_lexv.txt"
+    solution_path = "../solution-outputs/rosalind_lexv.txt"
     file = open(file_path, "r").readlines()
     A = str(file[0]).strip().replace(' ','')
     n = int(file[1])
