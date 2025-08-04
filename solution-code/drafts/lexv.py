a86143b594a8e26175bf88907102935fd99e6f27
robertbryan <rob@robertbryan.net>
2025-08-03 20:25:26 -0400

    solves challenge

--- solution-code/drafts/lexv.py
+++ solution-code/drafts/lexv.py
@@ -1,20 +1,26 @@
-import itertools
+import os
 
 def lexv(A, n):
-    result = []
-    for i in range(1, n+1):
-        for x in itertools.combinations_with_replacement(A, i):
-            result.append(str(''.join(x)))
-    return result
+    A = A[::-1]
+    stack = [(letter,) for letter in A]
 
-def mysort(n):
-    return A.find(n)
+    while stack:
+        string = stack.pop()
+        yield ''.join(string)
+        if len(string) != n:
+            for letter in A:
+                stack.append((*string, letter))
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_lexv.txt"
-    solution_path = "../solution-outputs/rosalind_lexv.txt"
+    solution_path = "../../solution-outputs/rosalind_lexv.txt"
     file = open(file_path, "r").readlines()
     A = str(file[0]).strip().replace(' ','')
     n = int(file[1])
-    answer = sorted((lexv(A, n)), key=mysort)
-    print(answer)
+    solution = list(lexv(A, n))
+    if os.path.exists(solution_path):
+        os.remove(solution_path) #idempotent : https://en.wikipedia.org/wiki/Idempotence
+    file = open(solution_path, "x")
+    for item in solution:
+        file.write(item + '\n')
+    file.close()
