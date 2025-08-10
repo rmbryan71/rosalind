a7dd2bf5a4a79128711a4c879c329d6dc07fc05b
robertbryan <rob@robertbryan.net>
2025-08-10 14:30:05 -0400

    solves challenge

---
+++ solution-code/long.py
@@ -0,0 +1,65 @@
+from Bio import SeqIO
+import sys, os
+
+def read_fasta(file):
+    return list(SeqIO.parse(file, 'fasta'))
+
+def min(a, b):
+    return a if a < b else b
+
+def overlap(str1, str2):
+    max_len = -sys.maxsize
+    len1 = len(str1)
+    len2 = len(str2)
+    str_ = ""
+    for i in range(1, min(len1, len2) + 1):
+        if str1[len1-i:] == str2[:i]:
+            if max_len < i:
+                max_len = i
+                str_ = str1 + str2[i:]
+    for i in range(1, min(len1, len2) + 1):
+        if str1[:i] == str2[len2-i:]:
+            if max_len < i:
+                max_len = i
+                str_ = str2 + str1[i:]
+    return max_len, str_
+
+def long(arr, n):
+    while n != 1:
+        max_len = -sys.maxsize
+        l, r = 0, 0
+        res_str = ""
+        for i in range(n):
+            for j in range(i+1, n):
+                str_ = ("")
+                res, str_ = overlap(arr[i], arr[j])
+                if max_len < res:
+                    max_len = res
+                    res_str = str_
+                    l, r = i, j
+        n -= 1
+        if max_len == -sys.maxsize:
+            arr[0] += arr[n]
+        else:
+            arr[l] = res_str
+            arr[r] = arr[n]
+    return arr[0]
+
+if __name__ == "__main__":
+    file_path = "/Users/robertbryan/Downloads/rosalind_long.txt"
+    solution_path = "../solution-outputs/rosalind_long.txt"
+    if os.path.exists(solution_path):
+        os.remove(solution_path)
+    sequences = []
+    for sequence in read_fasta(file_path):
+        sequences.append(str(sequence.seq))
+    # print(sequences)
+    n = len(sequences)
+    # print(n)
+    # print(long(sequences, n))
+    # print('ATTAGACCTGCCGGAATAC')
+    f = open(solution_path, "x")
+    f.write(long(sequences, n))
+    f.close()
+
+
