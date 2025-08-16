507aa57738ca344c33afd180ccdf62dfb2dfdd22
robertbryan <rob@robertbryan.net>
2025-08-16 12:21:25 -0400

    returns incorrect result

---
+++ solution-code/drafts/kmp.py
@@ -0,0 +1,22 @@
+from Bio import SeqIO
+
+def read_fasta(file):
+    return list(SeqIO.parse(file, 'fasta'))
+
+if __name__ == "__main__":
+    file_path = "/Users/robertbryan/Downloads/rosalind_kmp.txt"
+    solution_path = "../solution-outputs/rosalind_kmp.txt"
+    sequence = str(read_fasta(file_path)[0].seq)
+    print(sequence)
+    result = []
+    for i in range(1, len(sequence)):
+        if i == 1:
+            result.append(0)
+        if sequence[i]!=sequence[0]:
+            result.append(0)
+        else:
+            value = 1
+            while sequence[i+value] == sequence[value]:
+                value+=1
+            result.append(value)
+    print(result)
