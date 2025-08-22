04fd9396a77970c3953421db1270f23f3e2c95bf
robertbryan <rob@robertbryan.net>
2025-08-21 20:35:43 -0400

    solves challenge

---
+++ solution-code/seto.py
@@ -0,0 +1,48 @@
+import os
+
+if __name__ == "__main__":
+    file_path = "/Users/robertbryan/Downloads/rosalind_seto.txt"
+    solution_path = "../solution-outputs/rosalind_seto.txt"
+    # B, A, n = set(line.strip() for line in open(file_path))
+    file = open(file_path, 'r')
+    lines = file.readlines()
+    n = lines[0]
+    A = lines[1]
+    B = lines[2]
+
+    B = str(B).replace('{', '').replace('}', '')
+    ListB = []
+    for item in B.split(','):
+        ListB.append(int(item))
+    # print(ListB)
+    SetB = set(ListB)
+    # print(SetB)
+    A = str(A).replace('{', '').replace('}', '')
+    ListA = []
+    for item in A.split(','):
+        ListA.append(int(item))
+    # print(ListA)
+    SetA = set(ListA)
+    n = int(n)
+    ListN = []
+    for i in range(1, n + 1):
+        ListN.append(i)
+    SetN = set(ListN)
+    # n, SetA, SetB are populated at this point
+
+    if os.path.exists(solution_path):
+        os.remove(solution_path) #idempotent : https://en.wikipedia.org/wiki/Idempotence
+    file = open(solution_path, "x")
+    file.write(str(SetA.union(SetB)))
+    file.write('\n')
+    file.write(str(SetA.intersection(SetB)))
+    file.write('\n')
+    file.write(str(SetA.difference(SetB)))
+    file.write('\n')
+    file.write(str(SetB.difference(SetA)))
+    file.write('\n')
+    file.write(str(SetN.difference(SetA)))
+    file.write('\n')
+    file.write(str(SetN.difference(SetB)))
+    file.close()
+
