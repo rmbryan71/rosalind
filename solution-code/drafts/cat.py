a3e3ea33aed4ddc0d0170f3f5e732156bbebb29b
robertbryan <rob@robertbryan.net>
2025-08-24 13:24:54 -0400

    returns correct result for small datasets. Times out on large datasets

--- solution-code/drafts/cat.py
+++ solution-code/drafts/cat.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+import itertools
 
 def read_fasta(file):
     return list(SeqIO.parse(file, 'fasta'))
@@ -19,20 +20,62 @@ def maxchar(s):
     return max(s.count("A"), s.count("G"))
 
 def isComplement(a, b):
-    if a == "A" and b == 'U' or a == 'U' and b == 'A':
+    if a == 'A' and b == 'U':
         return True
-    if a == "C" and b == 'G' or a == 'G' and b == 'C':
+    if a == 'U' and b == 'A':
+        return True
+    if a == 'C' and b == 'G':
+        return True
+    if a == 'G' and b == 'C':
         return True
     else:
         return False
 
-def pairings(s):
-    result = []
+def matches(s):
+    s = list (s)
+    print(s)
+    matches = []
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if i != j:
+                if isComplement(s[i], s[j]):
+                    # print(i, j, s[i], s[j])
+                    if [j, i] not in matches:
+                        matches.append([i,j])
+    return matches
+
+def is_complete_match(s):
+    s = list (s)
     for i in range(len(s)-1):
+        x = s[i][0]
+        y = s[i][1]
         for j in range(i+1, len(s)):
-            if isComplement(s[i], s[j]):
-                result.append([i, j] + pairings(s[:i]+s[i+1:j]+s[j+1:]))
-    return result
+            if s[j][0] == x or s[j][1] == x:
+                return False
+            if s[j][0] == y or s[j][1] == y:
+                return False
+    return True
+
+def is_noncrossing(t):
+    t = list(t)
+    for edge_pair in itertools.combinations(t, 2):
+        i = int(edge_pair[0][0])
+        j = int(edge_pair[0][1])
+        k = int(edge_pair[1][0])
+        l = int(edge_pair[1][1])
+        # print(i, j, k, l)
+        if i < k < j < l:
+            return False
+        if j < k < i < l:
+            return False
+        if i > k > j > l:
+            return False
+        if j > k > i > l:
+            return False
+        if j < l < i < k:
+            return False
+    return True
+
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_cat_figure3.txt"
@@ -40,6 +83,15 @@ if __name__ == "__main__":
     nodes = []
     for char in s:
         nodes.append(char)
-    print(len(pairings(nodes)))
+    # print(matches(nodes))
+    count = 0
+    for subset in itertools.combinations(matches(nodes), len(nodes)//2):
+        if is_complete_match(subset):
+            print(subset, " is complete.")
+            if is_noncrossing(subset):
+                count += 1
+                print(subset, " is complete and non-crossing.")
+    print (count, " total")
+
 
 
