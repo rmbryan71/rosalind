08a95fc33ac7590e069de77eb937b716c95bc840
robertbryan <rob@robertbryan.net>
2025-08-27 05:41:11 -0400

    work in process with NumPy arrays

--- solution-code/drafts/edit.py
+++ solution-code/drafts/edit.py
@@ -13,6 +13,8 @@ def edit(s, t):
         d[i][0] = i
     for j in range(1, n):
         d[0][j] = j
+    print(d)
+
     for j in range (1, n):
         for i in range(1, m):
             if s[i] == t[j]:
@@ -20,11 +22,12 @@ def edit(s, t):
             else:
                 substCost = 1
             d[i][j] = min(d[i-1][j] + 1, d[i][j-1] +1, d[i-1][j-1] + substCost)
-    return d[m-1][n-1]
+    return d
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_edit.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_edit_wikipedia.txt"
     s = read_fasta(file_path)[0].seq
     t = read_fasta(file_path)[1].seq
-    print(s, t)
+    # print(s, t)
+    # print(len(s), len(t))
     print(edit(s, t))
