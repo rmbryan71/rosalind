d6124a6c3592ae0a3950555ba932c814ce1310e1
robertbryan <rob@robertbryan.net>
2025-08-27 05:22:40 -0400

    work in process with NumPy arrays

--- solution-code/drafts/edit.py
+++ solution-code/drafts/edit.py
@@ -1,4 +1,6 @@
 from Bio import SeqIO
+import numpy as np
+
 
 def read_fasta(file):
     return list(SeqIO.parse(file, 'fasta'))
@@ -6,12 +8,19 @@ def read_fasta(file):
 def edit(s, t):
     m = len(s)
     n = len(t)
-    d = [[0]*n]*m
+    d = np.full((m, n), 0)
     for i in range(1, m):
         d[i][0] = i
     for j in range(1, n):
         d[0][j] = j
-    return d
+    for j in range (1, n):
+        for i in range(1, m):
+            if s[i] == t[j]:
+                substCost = 0
+            else:
+                substCost = 1
+            d[i][j] = min(d[i-1][j] + 1, d[i][j-1] +1, d[i-1][j-1] + substCost)
+    return d[m-1][n-1]
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_edit.txt"
