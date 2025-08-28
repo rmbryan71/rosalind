7c08f3faf8f3f25445b3ccde125cec38d0241dda
robertbryan <rob@robertbryan.net>
2025-08-28 14:47:52 -0400

    work in progress: my version versions the math library

--- solution-code/drafts/aspc.py
+++ solution-code/drafts/aspc.py
@@ -10,13 +10,18 @@ def comb(n, k):
     return fact(n) // fact(k)*fact(n - k)
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_aspc.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_aspc_sample.txt"
     file = open(file_path, "r").readline().split(' ')
     n = int(file[0])
     m = int(file[1])
     print(n, m)
-    result = 0
+    result1 = 0
+    result2 = 0
     for i in range(m, n+1):
-        result += math.comb(n, i)
-        result %= 1000000
-    print(result)
+        result1 += math.comb(n, i)
+        result1 %= 1000000
+    for j in range(m, n+1):
+        result2 += comb(n, j)
+        result2 %= 1000000
+    print("Math library result = ", result1)
+    print("My equation result = ", result2)
