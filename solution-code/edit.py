3c5d9963f2e6163f7cbb35198c0e9edcf6aa5cc0
robertbryan <rob@robertbryan.net>
2025-08-27 20:49:12 -0400

    moves solution code from drafts folder

--- solution-code/edit.py
+++ solution-code/edit.py
@@ -8,26 +8,27 @@ def read_fasta(file):
 def edit(s, t):
     m = len(s)
     n = len(t)
-    d = np.full((m, n), 0)
-    for i in range(1, m):
-        d[i][0] = i
-    for j in range(1, n):
-        d[0][j] = j
-    print(d)
+    d = np.full((m+1, n+1), 0)
+    for i in range(0, m+1):
+        d[i, 0] = i
+    for j in range(0, n+1):
+        d[0, j] = j
+
 
     for j in range (1, n):
         for i in range(1, m):
-            if s[i] == t[j]:
+            if s[i-1] == t[j-1]:
                 substCost = 0
             else:
                 substCost = 1
-            d[i][j] = min(d[i-1][j] + 1, d[i][j-1] +1, d[i-1][j-1] + substCost)
-    return d
+            d[i,j] = min(d[i-1][j] + 1, d[i][j-1] +1, d[i-1][j-1] + substCost)
+    # print(d)
+    return d[m-1,n-1]
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_edit_wikipedia.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_edit.txt"
     s = read_fasta(file_path)[0].seq
     t = read_fasta(file_path)[1].seq
     # print(s, t)
-    # print(len(s), len(t))
+    print(len(s), len(t))
     print(edit(s, t))
