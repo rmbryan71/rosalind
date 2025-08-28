687a057fb829d36d3531e1018b8833e20883f26b
robertbryan <rob@robertbryan.net>
2025-08-28 15:01:31 -0400

    solves challenge

--- solution-code/aspc.py
+++ solution-code/aspc.py
@@ -7,21 +7,21 @@ def fact(n):
         return n * fact(n - 1)
 
 def comb(n, k):
-    return fact(n) // fact(k)*fact(n - k)
+    return fact(n) // (fact(k)*fact(n - k))
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_aspc_sample.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_aspc.txt"
     file = open(file_path, "r").readline().split(' ')
     n = int(file[0])
     m = int(file[1])
     print(n, m)
     result1 = 0
-    result2 = 0
+    # result2 = 0
     for i in range(m, n+1):
         result1 += math.comb(n, i)
         result1 %= 1000000
-    for j in range(m, n+1):
-        result2 += comb(n, j)
-        result2 %= 1000000
+    # for j in range(m, n+1):
+    #     result2 += comb(n, j)
+    #     result2 %= 1000000
     print("Math library result = ", result1)
-    print("My equation result = ", result2)
+    # print("My equation result = ", result2)
