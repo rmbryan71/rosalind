e69282e8c4fdfd567f3d8e7293a685a692a2b20b
robertbryan <rob@robertbryan.net>
2025-08-30 07:50:43 -0400

    work in progress

--- solution-code/drafts/trie.py
+++ solution-code/drafts/trie.py
@@ -1,77 +1,19 @@
-class TrieNode:
-    def __init__(self, text = ''):
-        self.text = text
-        self.children = dict()
-        self.is_word = False
-        self.id = int
+from itertools import groupby
 
-class PrefixTree:
-    def __init__(self):
-        self.root = TrieNode()
+def trie(seqs):
+    graph = {}
+    if len(seqs):
+        for base, nseqs in groupby(sorted(seqs), key=lambda s: s[0]):
+            graph[base] = trie([seq[1:] for seq in nseqs if len(seq) > 1])
+    return graph
 
-    def insert(self, word):
-        current = self.root
-        for i, char in enumerate(word):
-            if char not in current.children:
-                prefix = word[0:i+1]
-                current.children[char] = TrieNode(prefix)
-            current = current.children[char]
-        current.is_word = True
-
-    def __child_words_for(self, node, words):
-        '''
-        Private helper function. Cycles through all children
-        of node recursively, adding them to words if they
-        constitute whole words (as opposed to merely prefixes).
-        '''
-        if node.is_word:
-            words.append(node.text)
-        for letter in node.children:
-            self.__child_words_for(node.children[letter], words)
-
-    def starts_with(self, prefix):
-        '''
-        Returns a list of all words beginning with the given prefix, or
-        an empty list if no words begin with that prefix.
-        '''
-        words = list()
-        current = self.root
-        for char in prefix:
-            if char not in current.children:
-                # Could also just return words since it's empty by default
-                return list()
-            current = current.children[char]
-
-        # Step 2
-        self.__child_words_for(current, words)
-        return words
-
-    def crawl(self, prefix):
-        '''
-        Returns a list of all nodes beginning with the root, returning the node ID, child ID and text.
-        '''
-        nodes = list()
-        current = self.root
-        for char in prefix:
-            if char not in current.children:
-                # Could also just return words since it's empty by default
-                return list()
-            current = current.children[char]
-
-    def find(self, word):
-        '''
-        Returns the TrieNode representing the given word if it exists
-        and None otherwise.
-        '''
-        current = self.root
-        for char in word:
-            if char not in current.children:
-                return None
-            current = current.children[char]
-
-        # New code, None returned implicitly if this is False
-        if current.is_word:
-            return current
+def as_adjacency(graph, nodes=[]):
+    node = len(nodes) + 1
+    for edge in sorted(graph):
+        i = len(nodes) + 1
+        nodes.append((node, i + 1, edge))
+        nodes + as_adjacency(graph[edge], nodes)
+    return nodes
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_trie_sample.txt"
@@ -80,7 +22,6 @@ if __name__ == "__main__":
     for line in file:
         strings.append(line.strip())
     print(strings)
-    trie = PrefixTree()
-    for string in strings:
-        trie.insert(string)
-    print(trie.starts_with(''))
+    # print(trie(strings))
+    for edge in as_adjacency(trie(strings)):
+        print(*edge)
