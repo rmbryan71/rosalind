d8588c81d0f68f8866ebc633a0669e15da35df1a
robertbryan <rob@robertbryan.net>
2025-08-31 19:52:25 -0400

    WIP, generates a list of edges

--- solution-code/drafts/dbru.py
+++ solution-code/drafts/dbru.py
@@ -6,7 +6,7 @@ def reverse_complement(seq):
     return comp_seq[::-1]
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_dbru_sample.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_dbru.txt"
     file = open(file_path, "r").readlines()
     S = []
     for line in file:
@@ -15,11 +15,17 @@ if __name__ == "__main__":
     for sequence in S:
         reverse_complements.append(reverse_complement(sequence))
     for rc in reverse_complements:
-        S.append(rc)
+        if rc not in S:
+            S.append(rc)
     k = len(S[0])-1
     kmers = []
     for string in S:
         kmers.append(string[1:])
         kmers.append(string[:-1])
-    print(kmers)
+    # print(kmers)
+    count = 0
+    for edge in S:
+        count += 1
+        e = str((edge[:-1], edge[1:]))
+        print(e.replace("'", ""))
 
