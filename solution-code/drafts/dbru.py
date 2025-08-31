5c9c94f34f04e98859f5c4fc0d0cd81cd4027241
robertbryan <rob@robertbryan.net>
2025-08-31 19:28:22 -0400

    WIP, generates a list of kmers

---
+++ solution-code/drafts/dbru.py
@@ -0,0 +1,25 @@
+def reverse_complement(seq):
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    # Create the complement sequence
+    comp_seq = ''.join(complement[base] for base in seq)
+    # Return the reverse of the complement
+    return comp_seq[::-1]
+
+if __name__ == "__main__":
+    file_path = "/Users/robertbryan/Downloads/rosalind_dbru_sample.txt"
+    file = open(file_path, "r").readlines()
+    S = []
+    for line in file:
+        S.append(line.strip())
+    reverse_complements = []
+    for sequence in S:
+        reverse_complements.append(reverse_complement(sequence))
+    for rc in reverse_complements:
+        S.append(rc)
+    k = len(S[0])-1
+    kmers = []
+    for string in S:
+        kmers.append(string[1:])
+        kmers.append(string[:-1])
+    print(kmers)
+
