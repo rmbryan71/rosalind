9a21a1014bcef77bd4c03dea5c9369e435f02c0d
robertbryan <rob@robertbryan.net>
2025-09-01 19:06:10 -0400

    wip

--- solution-code/drafts/motz.py
+++ solution-code/drafts/motz.py
@@ -15,20 +15,11 @@ def isComplement(a, b):
     else:
         return False
 
-def catalan(dna, cata):
-    if dna in cata:
-        return cata[dna]
-    n = len(dna)
-    c = 0
-    for m in range(1, n ,2):
-        if isComplement(dna[0], dna[m]):
-            c += (catalan(dna[1:m], cata) * catalan(dna[m+1:], cata))
-    cata[dna] = c
-    return cata[dna]
+def motzkin(dna):
+    return False
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_motz_sample.txt"
     s = str(read_fasta(file_path)[0].seq).strip()
-    cata = {'': 1, 'AU': 1, 'UA': 1, 'GC': 1, 'CG': 1}
-    print(catalan(s, cata) % 1000000)
+    print(motzkin(s))
 
