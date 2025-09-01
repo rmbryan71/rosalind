0f868823370ab39c2656bfad46a508b33e80c9a6
robertbryan <rob@robertbryan.net>
2025-09-01 19:12:22 -0400

    wip

--- solution-code/drafts/motz.py
+++ solution-code/drafts/motz.py
@@ -16,7 +16,21 @@ def isComplement(a, b):
         return False
 
 def motzkin(dna):
-    return False
+    if len(dna) == 1:
+        return 1
+    if len(dna) == 0:
+        return 1
+    else:
+        return (
+            motzkin(dna[1:])
+            + sum(
+                motzkin(dna[1:m]) * motzkin(dna[m + 1 :])
+                for m in range(1, len(dna))
+                if isComplement(dna[0], dna[m])
+            )
+            % 1000000
+        )
+
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_motz_sample.txt"
