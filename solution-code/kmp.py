b72a8755134ffc1bf369bbdf59677abecd4a72b8
robertbryan <rob@robertbryan.net>
2025-09-04 06:42:01 -0400

    overhauls already solved code

--- solution-code/kmp.py
+++ solution-code/kmp.py
@@ -1,25 +1,14 @@
 from Bio import SeqIO
 
-
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_kmp.txt"
-    solution_path = "../../solution-outputs/rosalind_kmp.txt"
-    sequence_collection = []
-    for seq_record in SeqIO.parse(file_path, "fasta"):
-        sequence_collection.append(seq_record.seq)
-    sequence = sequence_collection.pop(0)
-    result = [0]*len(sequence)
-    position = 2
-    condition = 0
-    while position < len(sequence):
-        if sequence[position-1]==sequence[condition]:
-            condition+=1
-            result[position] = condition
-            position+=1
-        elif condition>0:
-            condition=result[condition]
-        else:
-            result[position]=0
-            position+=1
-    solution = str((result[1:], 0))
-    print((solution).replace('(', '').replace("[",'').replace(',','').replace(']','').replace(')',''))
+    for record in SeqIO.parse(file_path, "fasta"):
+        sequence = record.seq
+    result = [-1]
+    counter = -1
+    for i in range(len(sequence)):
+        while counter >= 0 and sequence[i] != sequence[counter]:
+            counter = result[counter]
+        counter += 1
+        result.append(counter)
+    print(*result[1:])
--- solution-code/sset.py
+++ solution-code/sset.py
@@ -1,6 +1,6 @@
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_sset.txt"
-    solution_path = "../solution-outputs/rosalind_sset.txt"
     file = open(file_path, "r").readline()
     n = int(file)
+    print(n)
     print((2**n)%1000000)
