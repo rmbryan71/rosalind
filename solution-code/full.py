c958207650e0eca40a72af006fd4af6c6973a8af
robertbryan <rob@robertbryan.net>
2025-09-12 18:30:30 -0400

    solves challenge

--- solution-code/full.py
+++ solution-code/full.py
@@ -30,7 +30,7 @@ def all_seq(ions, d):
     return res
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_full_sample.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_full.txt"
     file = open(file_path, "r").readlines()
     L = []
     for line in file:
