77d93dd0d4a7766f736fc6f048bd8bfea0163a11
robertbryan <rob@robertbryan.net>
2025-09-14 07:15:19 -0400

    wip with dbru approach

--- solution-code/drafts/pcov.py
+++ solution-code/drafts/pcov.py
@@ -1,51 +1,32 @@
-from Bio import SeqIO
-import sys, os
+def dbru(seqs):
+    return [(x[:-1], x[1:]) for x in seqs]
 
-def read_fasta(file):
-    return list(SeqIO.parse(file, 'fasta'))
 
-def min(a, b):
-    return a if a < b else b
+def find_cycle(graph):
+    key = sorted(list(graph.keys()))[0]
+    visited = set()
+    cycle = []
+    while key not in visited:
+        cycle += [key]
+        visited.add(key)
+        key = graph[key]
+    return cycle
 
-def overlap(str1, str2):
-    max_len = -sys.maxsize
-    len1 = len(str1)
-    len2 = len(str2)
-    str_ = ""
-    for i in range(1, min(len1, len2) + 1):
-        if str1[len1-i:] == str2[:i]:
-            if max_len < i:
-                max_len = i
-                str_ = str1 + str2[i:]
-    for i in range(1, min(len1, len2) + 1):
-        if str1[:i] == str2[len2-i:]:
-            if max_len < i:
-                max_len = i
-                str_ = str2 + str1[i:]
-    return max_len, str_
 
-def long(arr, n):
-    while n != 1:
-        max_len = -sys.maxsize
-        l, r = 0, 0
-        res_str = ""
-        for i in range(n):
-            for j in range(i+1, n):
-                str_ = ("")
-                res, str_ = overlap(arr[i], arr[j])
-                if max_len < res:
-                    max_len = res
-                    res_str = str_
-                    l, r = i, j
-        n -= 1
-        if max_len == -sys.maxsize:
-            arr[0] += arr[n]
-        else:
-            arr[l] = res_str
-            arr[r] = arr[n]
-    return arr[0]
+def join_cycle(chain):
+    return "".join(x[0] for x in chain)
+
+
+def pcov(seqs):
+    """Genome Assembly with Perfect Coverage"""
+    x = find_cycle(dict(dbru(seqs)))
+    return join_cycle(x)
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_pcov.txt"
     reads = []
+    with open(file_path) as file:
+        for line in file:
+            reads.append(str(line.strip()))
+    print(pcov(reads))
 
