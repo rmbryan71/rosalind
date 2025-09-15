c1f54df9adb1c5de8ef13c3987b336c9a2fcac6d
robertbryan <rob@robertbryan.net>
2025-09-14 20:19:25 -0400

    solves challenge

--- solution-code/nkew.py
+++ solution-code/nkew.py
@@ -2,7 +2,7 @@ from Bio import Phylo
 from io import StringIO
 
 if __name__ == "__main__":
-    file_path = "/Users/robertbryan/Downloads/rosalind_nkew_sample.txt"
+    file_path = "/Users/robertbryan/Downloads/rosalind_nkew.txt"
     with open(file_path, "r") as f:
         flines = f.readlines()
         for i in range(0, len(flines), 3):
