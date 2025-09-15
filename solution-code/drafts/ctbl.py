210686001ed5e8b087c2094ac2505e29c3563f68
robertbryan <rob@robertbryan.net>
2025-09-15 06:39:57 -0400

    wip

--- solution-code/drafts/ctbl.py
+++ solution-code/drafts/ctbl.py
@@ -1,38 +1,25 @@
 from Bio import Phylo
 from io import StringIO
-import re
 
-def nodes(graph):
-    s = list(graph.keys())
-    e = [y["n"] for v in graph.values() for y in v]
-    return set(s) | set(e)
+def ctbl(node, taxa):
+    for child in node.clades:
+        if not child.is_terminal():
+            arr = [0 for i in range(len(taxa))]
+            for child_node in child.find_clades():
+                if child_node.name:
+                    arr[taxa.index(child_node.name)] = 1
+            for elt in arr:
+                print(elt, end='')
+            print()
+            ctbl(child, taxa)
 
-def descendants(T):
-    def recurse(node):
-        if node not in T:
-            d[node] = []
-            return []
-        if node not in d:
-            children = [x["n"] for x in T[node]]
-            d[node] = children + [y for x in T[node] for y in recurse(x["n"])]
-            return d[node]
-
-    d = {}
-    recurse("0")
-    return d
-
-def ctbl(tree):
-    desc = descendants(tree)
-    allnodes = sorted(x for x in nodes(tree) if not re.match(r"^\d+$", x))
-    for subnodes in desc.values():
-        if not len(subnodes) or all([x in subnodes for x in allnodes]):
-            continue
-        yield "".join(["1" if n in subnodes else "0" for n in allnodes])
 
 if __name__ == "__main__":
     file_path = "/Users/robertbryan/Downloads/rosalind_ctbl_sample.txt"
-    with open(file_path, "r") as f:
-        handle = StringIO(f.readline().rstrip())
-        tree = Phylo.read(handle, 'newick')
-        for code in ctbl(tree):
-            print(code)
+    tree = Phylo.read(file_path, 'newick')
+    nodes = []
+    for node in tree.find_clades():
+        if node.name:
+            nodes.append(node.name)
+    nodes.sort()
+    ctbl(tree.root, nodes)
